b39ef84a75e319cb74b947ede83f0319c4ece45e
Rebecca <townsend.rebeccam@gmail.com>
2020-02-14 09:34:02 -0800

    try try again

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -39,7 +39,7 @@ export default function Contact() {
         method="post"
         action="/thanks/"
         data-netlify="true"
-        data-netlify-honeypot="bot-field"
+        // data-netlify-honeypot="bot-field"
         onSubmit={handleSubmit}
       >
         {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
